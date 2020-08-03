8c9c30f0562ee78156b211a094dd2fd7c7d8883e
kalllllor <karolgren3@gmail.com>
2020-08-03 16:24:39 +0200

    react route finished

--- reacta_podstawy/src/components/List/ListItem.js
+++ reacta_podstawy/src/components/List/ListItem.js
@@ -1,39 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ListItem.module.scss";
+import Button from '../Button/Button';
+import Title from '../Title/Title';
 
 const ListItem = ({ image, name, description, twitterLink }) => {
-  const ImageTag = image ? "img" : "div"
+  const ImageTag = image ? "img" : "div";
+
   return (
     <li className={styles.wrapper}>
-      <ImageTag src={image} className={image ? styles.image : styles.imageNone} alt={name} />
+      <ImageTag 
+        src={image} 
+        className={image ? styles.image : styles.imageNone} 
+        alt={name}
+      />
       <div>
-        <h2 className={styles.name}>{name}</h2>
+        <Title>{name}</Title>
         <p className={styles.description}>{description}</p>
-        <a
+        <Button
           href={twitterLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.button}>
+        >
           visit twitter page
-      </a>
+        </Button>
       </div>
     </li>
-  )
-}
-
-
+  );
+};
 
 ListItem.propTypes = {
   image: PropTypes.string,
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
-  twitterLink: PropTypes.string.isRequired
+  twitterLink: PropTypes.string.isRequired,
 };
 
 ListItem.defaultProps = {
+  image: null,
   description: "One of the React creators",
-  image: "https://f4.bcbits.com/img/a1519626822_16.jpg"
 };
 
 export default ListItem;
