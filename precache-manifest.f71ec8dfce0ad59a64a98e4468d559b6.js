7eb55248c67415fb84cdeda14a463ab7c73f2648
kalllllor <karolgren3@gmail.com>
2020-09-15 17:10:56 +0200

    Updates

---
+++ precache-manifest.f71ec8dfce0ad59a64a98e4468d559b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a97aa4901b6183c447dbef26cf522b",
+    "url": "/Kurs-React-podstawy/index.html"
+  },
+  {
+    "revision": "816c6518d956e59f29da",
+    "url": "/Kurs-React-podstawy/static/css/main.3c6eda33.chunk.css"
+  },
+  {
+    "revision": "cda0f7410f13707660cc",
+    "url": "/Kurs-React-podstawy/static/js/2.76eb46c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Kurs-React-podstawy/static/js/2.76eb46c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816c6518d956e59f29da",
+    "url": "/Kurs-React-podstawy/static/js/main.12769c58.chunk.js"
+  },
+  {
+    "revision": "7e843dbe8b78f96f52a8",
+    "url": "/Kurs-React-podstawy/static/js/runtime-main.04d208a8.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/Kurs-React-podstawy/static/media/logo.a87d0f35.svg"
+  }
+]);
